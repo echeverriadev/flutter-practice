83797d96b630721290682623cf8b074a75d92b88
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-12 15:18:07 -0400

    implemint view with a statefullwidget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
   final textStyle = new TextStyle( fontSize: 25);
+  final conteo =10;
 
   @override
   Widget build(context) {
@@ -15,13 +16,15 @@ class HomePage extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.center, //propiedad de column para centrar los hijos
           children: <Widget>[
             Text("Número de clicks", style: textStyle), 
-            Text("0", style: textStyle)
+            Text("$conteo", style: textStyle)
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
         print("HOLAA SI FUNCIONO");
+        //conteo ++;
       }, child: Icon(Icons.add),)
+      
     ) ;
   }
 }
