86966cff493aaec15e50fd629c97797d7ef4246d
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-11 20:58:52 -0400

    adding folder to the structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/src/app.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build (context){
-    return MaterialApp(
-      home:Center(
-        child: Text("HELLO WORLD")
-      )
-    );
-  }
-}
