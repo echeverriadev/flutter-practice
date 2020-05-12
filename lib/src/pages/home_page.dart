1d99e81ee69e939a4d60c766aa60b0cd607e8a04
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-11 22:23:15 -0400

    add more widget with style

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+  final textStyle = new TextStyle( fontSize: 25);
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("APP BAR"),
+        title: Text("CONTADOR"),
         centerTitle: true,
       ),
       body: Center(
-        child: Text("HELLO WORLD"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //propiedad de column para centrar los hijos
+          children: <Widget>[
+            Text("Número de clicks", style: textStyle), 
+            Text("0", style: textStyle)
+          ],
+        ),
       ),
     ) ;
   }
